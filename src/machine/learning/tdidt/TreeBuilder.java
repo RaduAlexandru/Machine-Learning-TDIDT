7fe3fcb548cd3d282c1aaad2ee0b75bf2de0f7a1
RaduAlexandru <alejandro.rosu@gmail.com>
2015-11-17 23:58:22 +0100

    Classes made, and the pseudocode is finished, needs only implementing.

---
+++ src/machine/learning/tdidt/TreeBuilder.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package machine.learning.tdidt;
+
+/**
+ *
+ * @author Alex
+ */
+class TreeBuilder {
+
+    void create(Node root) {
+        //If
+        
+    }
+    
+}
